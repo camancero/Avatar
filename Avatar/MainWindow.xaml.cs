03d0a184b04048f8ab5f3cbbd36b228a1c5fa352
Nathan <nathansteadman@live.co.uk>
2017-09-28 08:23:56 +0100

    y u no math 2

--- Avatar/MainWindow.xaml.cs
+++ Avatar/MainWindow.xaml.cs
@@ -5,8 +5,11 @@ using System.Windows.Media.Media3D;
 using System.IO;
 using System;
 using System.Collections.Generic;
-using System.Timers;
 using NUClass;
+using hello;
+using MathWorks.MATLAB.NET.Arrays;
+using MathWorks.MATLAB.NET.Utility;
+using System.Linq;
 
 namespace Avatar
 {
@@ -20,51 +23,16 @@ namespace Avatar
         string input_data = @"C:\Users\Nathan\AppData\Local\M-Mark\M-Mark\ExerciseData\Patient - practice-3Reps - 1-Hold phone - Right-2017-09-09T13-21-46.csv";
         public List<Quaternion[]> initial_quaternions = new List<Quaternion[]>();
         private readonly Quaternion offset = new Quaternion(0, 0.7071067811865475, 0.7071067811865476, 0);
-        private Quaternion ninetyx = new Quaternion(0,0.7071,    0.7071,         0);
+        private Quaternion ninetyx = new Quaternion(0, 0.7071, 0.7071, 0);
         ConnectionManager cm = new ConnectionManager();
 
-        /// <summary>
-        /// The coordinates of the center of rotation of the torso
-        /// </summary>
-        private double[] torso_centroid = { 0.83, 4.63, 56.9 };
-        private double[] right_UA_centroid = { 27.8, 4.89, 100 };
-        private double[] right_FA_centroid = { 27.8, 4.89, 54.4 };
-        private double[] right_hand_centroid = { 27.8, 4.89, 8.32 };
+
         private List<double[]> centroids = new List<double[]>();
         private Quaternion[] initial_frame = new Quaternion[4];
 
+         
 
-
-        /// <summary>
-        /// The coordinates of the center of rotation of the Left upper arm (i.e. left shoulder)
-        /// </summary>
-        private const double LeftUpperarmCenterOfRotationX = -29.1, LeftUpperarmCenterOfRotationY = 4.89, LeftUpperarmCenterOfRotationZ = 100;
-
-        /// <summary>
-        /// The coordinates of the center of rotation of the Right upper arm (i.e right shoulder)
-        /// </summary>
-        private const double RightUpperarmCenterOfRotationX = 27.8, RightUpperarmCenterOfRotationY = 4.89, RightUpperarmCenterOfRotationZ = 100;
-
-        /// <summary>
-        /// The coordinates of the center of rotation of the Left Forearm (i.e. left elbow)
-        /// </summary>
-        private const double LeftForearmCenterOfRotationX = -29.1, LeftForearmCenterOfRotationY = 4.89, LeftForearmCenterOfRotationZ = 54.4;
-
-        /// <summary>
-        /// The coordinates of the center of rotation of the Right Forearm (i.e. right elbow)
-        /// </summary>
-        private const double RightForearmCenterOfRotationX = 27.8, RightForearmCenterOfRotationY = 4.89, RightForearmCenterOfRotationZ = 54.4;
-
-        /// <summary>
-        /// The coordinates of the center of rotation of the Left Hand (i.e. left wrist)
-        /// </summary>
-        private const double LeftHandCenterOfRotationX = -29.1, LeftHandCenterOfRotationY = 4.89, LeftHandCenterOfRotationZ = 8.32;
-
-        /// <summary>
-        /// The coordinates of the center of rotation of the Right Hand (i.e. right wrist)
-        /// </summary>
-        private const double RightHandCenterOfRotationX = 27.8, RightHandCenterOfRotationY = 4.89, RightHandCenterOfRotationZ = 8.32;
-        /// <summary>
+       
         /// The 3D model group containing the components for the model
         /// </summary>
         private Model3DGroup skeleton;
@@ -91,14 +59,13 @@ namespace Avatar
 
         ProcessedDataReader pdr;
         ModelVisual3D modelVisual3D;
-        private Timer aTimer;
         System.Windows.Threading.DispatcherTimer animationTimer;
         private int step = 1;
         public Model3D AvatarModel { get; set; }
         public Model3D[] components;
         public Quaternion[] orientations = new Quaternion[4];
-        public Quaternion[] previous = new Quaternion[4];
-        int[] count = {1,1,1,1};
+        public Quaternion[] reference = new Quaternion[4];
+        int[] count = { 2, 2, 2, 2 };
         int[] ready = { 0, 0, 0, 0 };
 
 
@@ -109,23 +76,18 @@ namespace Avatar
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            centroids.Add(torso_centroid);
-            centroids.Add(right_UA_centroid);
-            centroids.Add(right_FA_centroid);
-            centroids.Add(right_hand_centroid);
-            orientations[0] = Quaternion.Identity;
-            orientations[1] = Quaternion.Identity;
-            orientations[2] = Quaternion.Identity;
-            orientations[3] = Quaternion.Identity;
-            previous[0] = Quaternion.Identity;
-            previous[1] = Quaternion.Identity;
-            previous[2] = Quaternion.Identity;
-            previous[3] = Quaternion.Identity;
+            this.m_helix_viewport.ModelUpDirection = new Vector3D(0, 1, 0);
+
+            //helloClass X = new helloClass();
+            
+            //MWArray[] result = X.hello(1);
+            //System.Diagnostics.Debug.WriteLine((MWNumericArray)result[0]);
+            populate_reference_data();
             initial_frame[0] = new Quaternion(-0.330925973595304, -0.29698484809835, 0.524673231640418, 0.725491557497398);
-            initial_frame[1] = new Quaternion(- 0.323147799002252,0.173241161390704,0.143542676580869,-0.918531708761325);
+            initial_frame[1] = new Quaternion(-0.323147799002252, 0.173241161390704, 0.143542676580869, -0.918531708761325);
             initial_frame[2] = new Quaternion(0.580534667354156, -0.00565685424949242, 0.410121933088198, -0.703571247280615);
             initial_frame[3] = new Quaternion(0.399515331370399, -0.20081832585698, 0.526087445202791, -0.723370237153838);
-            System.Diagnostics.Debug.WriteLine("FIRED");
+
             //The Importer to load .obj files
             ModelImporter importer = new ModelImporter();
 
@@ -142,175 +104,180 @@ namespace Avatar
             rightForearm = importer.Load(Path.Combine(components_dir, "rightForeArm.stl"));
             rightHand = importer.Load(Path.Combine(components_dir, "rightHand.stl"));
 
+            
             skeleton.Children.Add(waist);
             skeleton.Children.Add(torso);
             skeleton.Children.Add(rightUpperArm);
             skeleton.Children.Add(rightForearm);
-            skeleton.Children.Add(rightHand);
+            //skeleton.Children.Add(rightHand);
 
-            components = new Model3D[] { torso, rightUpperArm, rightForearm, rightHand};
+            components = new Model3D[] { torso, rightUpperArm, rightForearm, rightHand };
 
             var componentTransform = new Transform3DGroup();
 
             RotateTransform3D initalRotationZ = new RotateTransform3D(new AxisAngleRotation3D(new Vector3D(0, 0, 1), 100));
-            initalRotationZ.CenterX = torso_centroid[0];
-            initalRotationZ.CenterY = torso_centroid[1];
-            initalRotationZ.CenterZ = torso_centroid[2];
+            initalRotationZ.CenterX = centroids[0][0];
+            initalRotationZ.CenterY = centroids[0][1];
+            initalRotationZ.CenterZ = centroids[0][2];
 
-           // componentTransform.Children.Add(initalRotationZ);
+            //componentTransform.Children.Add(initalRotationZ);
 
             RotateTransform3D initalRotationX = new RotateTransform3D(new AxisAngleRotation3D(new Vector3D(1, 0, 0), 20));
-            initalRotationX.CenterX = torso_centroid[0];
-            initalRotationX.CenterY = torso_centroid[1];
-            initalRotationX.CenterZ = torso_centroid[2];
+            initalRotationX.CenterX = centroids[0][0];
+            initalRotationX.CenterY = centroids[0][1];
+            initalRotationX.CenterZ = centroids[0][2];
 
-           // componentTransform.Children.Add(initalRotationX);
+            //componentTransform.Children.Add(initalRotationX);
 
             skeleton.Transform = componentTransform;
 
             modelVisual3D = new ModelVisual3D();
             modelVisual3D.Content = skeleton;
             this.m_helix_viewport.Children.Add(modelVisual3D);
-        
+
 
             Avatar.DataContext = this;
             //pdr = new ProcessedDataReader(input_data);
 
-            var x = torso.Transform.Value;
-            var y = rightForearm.Transform.Value;
-            int i = 0;
-
         }
 
-        public void button(object sender, RoutedEventArgs e)
+        private void populate_reference_data()
+        {
+            //TODO:  LEFT FOREARM Z= 54.4
+            centroids.Add(new double[]{ 0.83, 4.63, 56.9 }); // Torso
+            centroids.Add(new double[] { 27.8, 4.89, 100 }); // Right Upper Arm
+            centroids.Add(new double[] { 27.8, 4.89, 54.4 }); // Right Forearm
+            centroids.Add(new double[] { 27.8, 4.89, 8.32 }); // Right Hand
+            centroids.Add(new double[] { -29.1, 4.89, 100 }); // Left Upper Arm
+            centroids.Add(new double[] { -29.1, 4.89, 54.4 }); // Left Forearm
+            centroids.Add(new double[] { -29.1, 4.89, 8.32 }); // Left Hand
+
+
+    }
+
+    public void button(object sender, RoutedEventArgs e)
         {
             System.Diagnostics.Debug.WriteLine("BUTTOn");
             animationTimer = new System.Windows.Threading.DispatcherTimer();
             animationTimer.Interval = new TimeSpan(50 * 1000);
             animationTimer.Tick += OnTimedEvent;
             animationTimer.Start();
-         
+        }
 
 
+        private Quaternion chest(Quaternion q)
+        {
+            Quaternion rotation_a = new Quaternion(new Vector3D(0, 0, 1), -90);
+            Quaternion rotation_a_conj = rotation_a;
+            rotation_a_conj.Conjugate();
+            Quaternion rot = Quaternion.Multiply(Quaternion.Multiply(rotation_a, q), rotation_a_conj);
+            return rot;
+        }
+
+        private Quaternion UA(Quaternion q)
+        {
+            Quaternion offset = Quaternion.Multiply(q, offsets[1]);
+            return offset;
+            
+            /*
+            Quaternion rotation_a = new Quaternion(new Vector3D(0, 1, 0), 45);
+            Quaternion rotation_a_conj = rotation_a;
+            rotation_a_conj.Conjugate();
+            Quaternion rot = Quaternion.Multiply(Quaternion.Multiply(rotation_a, q), rotation_a_conj);
+           
+            
+            Quaternion x = new Quaternion(q.Y, -q.X, q.Z, q.W);
+
+            Quaternion rotation_b = new Quaternion(new Vector3D(0, 0, 1), -45);
+            Quaternion rotation_b_conj = rotation_b;
+            rotation_a_conj.Conjugate();
+            Quaternion man = Quaternion.Multiply(Quaternion.Multiply(rotation_b, x), rotation_b_conj);
+            System.Diagnostics.Debug.WriteLine(q + " // " + rot + " // " + man);
+            return man; 
+            */
 
         }
 
-        private Quaternion wrist(Quaternion input)
+        private Quaternion FA(Quaternion q)
         {
-            Quaternion mani = new Quaternion(-input.Y, input.X, input.Z, input.W);
+            Quaternion x = new Quaternion(q.Y, -q.X, q.Z, q.W);
+
+            Quaternion rotation_a = new Quaternion(new Vector3D(0, 0, 1), -45);
+            Quaternion rotation_a_conj = rotation_a;
+            rotation_a_conj.Conjugate();
+            return Quaternion.Multiply(Quaternion.Multiply(rotation_a, x), rotation_a_conj);
+        }
+
+        private Quaternion WR(Quaternion q)
+        {
             Quaternion rot = Quaternion.Identity;
-            if (input != Quaternion.Identity)
+            if (q != Quaternion.Identity)
             {
                 Quaternion rotation_a = new Quaternion(new Vector3D(0, 0, 1), -90);
                 Quaternion rotation_a_conj = rotation_a;
                 rotation_a_conj.Conjugate();
-                rot = Quaternion.Multiply(Quaternion.Multiply(rotation_a, input), rotation_a_conj);
-
-                Quaternion rotation_b = new Quaternion(new Vector3D(0, 0, 1), 180);
-                Quaternion rotation_b_conj = rotation_b;
-                rotation_b_conj.Conjugate();
-                rot = Quaternion.Multiply(Quaternion.Multiply(rotation_b, rot), rotation_b_conj);
+                rot = Quaternion.Multiply(Quaternion.Multiply(rotation_a, q), rotation_a_conj);
             }
-            System.Diagnostics.Debug.WriteLine(rot + "  ///  " + mani);
             return rot;
         }
 
-        private Quaternion chest(Quaternion input)
-        {
-
-            return input;
-        }
-
-        private Quaternion upperarm(Quaternion input)
-        {
-           Quaternion mani =  new Quaternion(-input.Y, input.X, input.Z, input.W);
-            Quaternion rot = Quaternion.Identity;
-            if (input != Quaternion.Identity)
-            {
-                Quaternion rotation_a = new Quaternion(new Vector3D(0, 0, 1), -65);
-                Quaternion rotation_a_conj = rotation_a;
-                rotation_a_conj.Conjugate();
-                rot = Quaternion.Multiply(Quaternion.Multiply(rotation_a, input),rotation_a_conj);
-
-                Quaternion rotation_b = new Quaternion(new Vector3D(0,0, 1), 180);
-                Quaternion rotation_b_conj = rotation_b;
-                rotation_b_conj.Conjugate();
-                rot = Quaternion.Multiply(Quaternion.Multiply(rotation_b, rot),rotation_b_conj);
-            }
-            System.Diagnostics.Debug.WriteLine(rot + "  ///  " + mani);
-            return rot;
-        }
-
-        private Quaternion forearm(Quaternion input)
-        {
-            Quaternion mani = new Quaternion(-input.Y, input.X, input.Z, input.W);
-            Quaternion rot = Quaternion.Identity;
-            if (input != Quaternion.Identity)
-            {
-                Quaternion rotation_a = new Quaternion(new Vector3D(0, 0, 1), -65);
-                Quaternion rotation_a_conj = rotation_a;
-                rotation_a_conj.Conjugate();
-                rot = Quaternion.Multiply(Quaternion.Multiply(rotation_a, input), rotation_a_conj);
-
-                Quaternion rotation_b = new Quaternion(new Vector3D(0, 0, 1), 180);
-                Quaternion rotation_b_conj = rotation_b;
-                rotation_b_conj.Conjugate();
-                rot = Quaternion.Multiply(Quaternion.Multiply(rotation_b, rot), rotation_b_conj);
-            }
-            System.Diagnostics.Debug.WriteLine(rot + "  ///  " + mani);
-            return rot;
-
-
-
-        }
-
-
+        private Quaternion[] offsets = new Quaternion[4];
 
         private void OnTimedEvent(object source, EventArgs e)
         {
             Dispatcher.Invoke(() =>
             {
-                //System.Diagnostics.Debug.WriteLine(step);
-                /*
-                if (step == pdr.quaternions.Count - 1)
-                {
-                    animationTimer.Stop();
-                    return;
-                }
-                */
                 int[] order = {2,0,1,3 };
             for (var x = 0; x < 4; x++) {
-                    /*
-                Quaternion from = pdr.initial_quaternions[order[x]]; 
-                Quaternion to = pdr.quaternions[step + 1][order[x]];
-               // Quaternion delta = Quaternion.Subtract(to, from);
-                    from.Invert();
-                    Quaternion delta2 = Quaternion.Multiply(to, from);
-                    */
                     Quaternion current = cm.current_quaternions[order[x]];
-                    //ready[x] = cm.is_ready(order[x]);
-                    if (previous[x] == Quaternion.Identity) //previous[x] == Quaternion.Identity)
+                    if(current != Quaternion.Identity)
                     {
-                        animate(Quaternion.Identity, x);
-                        previous[x] = current; // Quaternion.Multiply(offset, current); 
-                    } else
-                    {
-                        if(count[x] == 1)
+                        //current.Normalize();
+                        if (x == 0)
                         {
-                            System.Diagnostics.Debug.WriteLine("BEGIN");
-                            count[x] = 0;
+                            current = chest(current);
+                        }
+                        
+                        if (count[x] == 2) //If first read
+                        {
+                            reference[x] = current;
+                            count[x] = 1;
+                            System.Diagnostics.Debug.WriteLine(x + ": Reference Set.");
+                        } else if (count.Sum() <= 4)
+                        {
+
+                            if(count[x] == 1)
+                            {
+                                Quaternion chest_ref = reference[0];
+                                chest_ref.Invert();
+                                offsets[x] =  Quaternion.Multiply(reference[0], chest_ref); 
+                                if(x == 0)
+                                {
+                                    //offsets[x] = Quaternion.Identity;
+                                }
+                                count[x] = 0;
+                                System.Diagnostics.Debug.WriteLine(x + ": Offset Calculated. " + (offsets[x] ==Quaternion.Identity) + " - " +  offsets[x] + " - " + reference[x]);
+                            } else if (count[x] == 0)
+                            {
+
+                        Quaternion ref_frame = reference[x];
+                        ref_frame.Invert();
+                        
+                                Quaternion offset = Quaternion.Multiply(current, offsets[x]);
+                                Quaternion delta = Quaternion.Multiply(offset, ref_frame);
+                                animate(delta, x);
+               
+                            }
+
+
                         }
 
-                        Quaternion last = previous[x];
-                        last.Invert();
-                        Quaternion delta = Quaternion.Multiply(current, last);
-                        //orientations[x] = Quaternion.Multiply(orientations[x], delta);
-                        animate(delta, x);
-                        //previous[x] = current;
                     }
+                    
 
+
+         
                 }
-            step++;
             });
         }
 
@@ -336,23 +303,26 @@ namespace Avatar
             TranslateTransform3D origin_translation = new TranslateTransform3D(new Vector3D(origin.X - original_origin.X, origin.Y - original_origin.Y, origin.Z - original_origin.Z));
             componentTransform.Children.Add(origin_translation);
             }
-            if (x == 3)
+
+            switch (x)
             {
-                quat = wrist(quat) ;
-                //System.Diagnostics.Debug.WriteLine(quat);
+                case 0:
+                    //quat = chest(quat);
+                    break;
+                case 1:
+                   // quat = UA(quat);
+                    break;
+                case 2:
+                  //  quat = FA(quat);
+                    break;
+                case 3:
+                   // quat = WR(quat);
+                    break;
+                default:
+                    Console.WriteLine("Wut?");
+                    break;
             }
-            if(x == 0)
-            {
-                quat = chest(quat);
-            }
-            if(x == 1)
-            {
-                quat = upperarm(quat);
-            }
-            if(x == 2)
-            {
-                quat = forearm(quat);
-            }
+
             RotateTransform3D transform = new RotateTransform3D(new QuaternionRotation3D(quat));
 
             // The point of rotation
@@ -376,7 +346,7 @@ namespace Avatar
 
         public void connect(object sender, RoutedEventArgs e)
         {
-            cm.connection_event();
+          cm.connection_event();
         }
 
         public void stream(object sender, RoutedEventArgs e)
